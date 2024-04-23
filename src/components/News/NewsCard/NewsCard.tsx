03b91c09413c6ca0cbb4d1904c9ed1cc529a7b40
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-04-23 12:54:44 +0300

    init

--- src/components/News/NewsCard/NewsCard.tsx
+++ src/components/News/NewsCard/NewsCard.tsx
@@ -11,7 +11,9 @@ const NewsCard = (props: INew) => {
     const [ hover, setHover ] = React.useState(false);
     const formattedToDate = props.date.format('DD MMMM');
     return <Link to={props.link} className={styles.wrapper} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-        <motion.div className={styles.img} animate={{ scale: hover ? 1.1 : 1, marginLeft: hover && props.id !== 1 && props.id !== 3 ? '10px' : '0', width: props.id === 1 || props.id === 3 ? '100%' : '80%', height: props.id === 1 || props.id === 3 ? '50%' : '60%', marginTop: hover ? '20px': '0'}} style={{ backgroundImage: `url(${props.image})`,  }} />
+        <motion.div className={styles.img}
+                    // animate={{ scale: hover ? 1.1 : 1, marginLeft: hover && props.id !== 1 && props.id !== 3 ? '10px' : '0', width: props.id === 1 || props.id === 3 ? '100%' : '80%', height: props.id === 1 || props.id === 3 ? '50%' : '60%', marginTop: hover ? '20px': '0'}}
+                    style={{ backgroundImage: `url(${props.image})`,  }} />
         <p>
             <p style={{ marginBottom: '20px'}}>{formattedToDate}</p>
             <h1 style={props.id === 1 || props.id === 3 ? { fontFamily:  '"TT Norms", sans-serif', fontSize: '14px'} : {}}>{props.title}</h1>
